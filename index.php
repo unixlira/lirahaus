759c582122b3449250b0b5665d9095fcdb39eca0
Jose Lira <joserobertolira@gmail.com>
2019-10-31 20:04:54 -0300

    CreatedAt repository PousadaLiraHaus

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+phpinfo();
+// require_once("vendor/autoload.php");
+
+// $app = new \Slim\Slim();
+
+// $app->config('debug', true);
+
+// $app->get('/', function() {
+    
+// 	echo "OK";
+
+// });
+
+// $app->run();
+
+ ?>
